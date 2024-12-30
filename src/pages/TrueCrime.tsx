186dd30a80c6df91408d97a53472de2040bd7d98
bandofcutters <121744307+bandofcutters@users.noreply.github.com>
2024-12-30 02:47:59 -0500

    Update TrueCrime.tsx
    Removed Trailer from Watch Trailer

--- src/pages/TrueCrime.tsx
+++ src/pages/TrueCrime.tsx
@@ -61,7 +61,7 @@ const TrueCrime = () => {
                   asChild
                 >
                   <a href={show.trailerUrl} target="_blank" rel="noopener noreferrer">
-                    <Play className="mr-2" /> Watch Trailer
+                    <Play className="mr-2" /> Watch
                   </a>
                 </Button>
               </div>
